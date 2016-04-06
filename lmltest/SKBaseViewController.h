3e2b242458a94241afea4e29dac8bdce2f9eb668
liuminli <liuminli@19where.com>
2016-04-06 17:34:54 +0800

    修改

--- lmltest/SKBaseViewController.h
+++ lmltest/SKBaseViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "SKHeader.h"
 @interface SKBaseViewController : UIViewController
 
 @end
---
+++ lmltest/SKBaseViewController.m
@@ -0,0 +1,39 @@
+//
+//  SKBaseViewController.m
+//  SKUserSDK
+//
+//  Created by lml on 16/3/22.
+//  Copyright © 2016年 lml. All rights reserved.
+//
+
+#import "SKBaseViewController.h"
+
+@interface SKBaseViewController ()
+
+@end
+
+@implementation SKBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor whiteColor];
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
