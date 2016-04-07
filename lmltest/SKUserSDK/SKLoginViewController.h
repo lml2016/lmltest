12741145c553c34a99e07f9920ada446796d4d01
liuminli <liuminli@19where.com>
2016-04-07 16:04:19 +0800

    7

---
+++ lmltest/SKUserSDK/SKLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  SKLoginViewController.h
+//  SKUserSDK
+//
+//  Created by lml on 16/3/22.
+//  Copyright © 2016年 lml. All rights reserved.
+//
+
+#import "SKBaseViewController.h"
+
+@interface SKLoginViewController : SKBaseViewController
+@property (nonatomic,strong) UIViewController * nextController;
+@end
