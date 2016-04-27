17f4b5dbec49b0f5f6536c33d9c5ef0b823875c8
liuminli <liuminli@19where.com>
2016-04-27 15:44:52 +0800

    只有xib

---
+++ lmltest/BaseController.h
@@ -0,0 +1,14 @@
+//
+//  BaseController.h
+//  suiyi
+//
+//  Created by lml on 16/4/20.
+//  Copyright © 2016年 lml. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseController : UIViewController
+@property (weak, nonatomic) IBOutlet UILabel *Label;
+
+@end
